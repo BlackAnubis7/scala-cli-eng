59507de000f98c4a6b53aafb1414c4916f7c5c04
Anubis <github@anubis.email>
2022-07-18 15:35:41 +0200

    avoid splitting scopes where not necessary

--- modules/cli/src/main/scala/scala/cli/commands/mdsandbox/SnippetPackager.scala
+++ modules/cli/src/main/scala/scala/cli/commands/mdsandbox/SnippetPackager.scala
@@ -1,6 +1,8 @@
 package scala.cli.commands.mdsandbox
 
-class SnippetPackager(val fileName: String, val snippets: Seq[MarkdownSnippet.Fence]) {
+import MarkdownSnippet.Fence
+
+class SnippetPackager(val fileName: String, val snippets: Seq[Fence]) {
   val fileIdentifier = fileName.takeWhile(c => c.isLetterOrDigit || c == '_')
 
   def className(index: Int): String = s"Snippet$index"
@@ -9,7 +11,9 @@ class SnippetPackager(val fileName: String, val snippets: Seq[MarkdownSnippet.Fe
     (0 until snippets.length).foldLeft(
       s"object markdown_$fileIdentifier {def main(args: Array[String]): Unit = {"
     ) (
-      (sum, index) => sum :++ s"new ${className(index)}; "
+      (sum, index) => 
+        if (snippets(index).resetScope || index == 0) sum :++ s"new ${className(index)}; "
+        else sum  // that class hasn't been created
     )
     .:++("} ")
     .:++(buildScalaMain(0, 0))
@@ -19,12 +23,16 @@ class SnippetPackager(val fileName: String, val snippets: Seq[MarkdownSnippet.Fe
   private def buildScalaMain(index: Int, line: Int): String = {
     if (index >= snippets.length) ""
     else {
-      val fence: MarkdownSnippet.Fence = snippets(index)
-      ("\n" * (fence.startLine - line - 1))  // padding
-        .:++(s"class ${className(index)} {\n")
-        .:++(fence.body)
-        .:++("\n}")
-        .:++(buildScalaMain(index + 1, fence.endLine + 1))
+      val fence: Fence = snippets(index)
+      val classOpener: String =
+        if (index == 0)            s"class ${className(index)} {\n"    // first snippet needs to open a class
+        else if (fence.resetScope) s"} class ${className(index)} {\n"  // if scope is being reset, close previous class and open a new one
+        else "\n"
+      ("\n" * (fence.startLine - line - 1))                 // padding
+        .:++(classOpener)                                   // new class opening (if applicable)
+        .:++(fence.body)                                    // snippet body
+        .:++("\n")                                          // padding in place of closing backticks
+        .:++(buildScalaMain(index + 1, fence.endLine + 1))  // further snippets
     }
   }
 }
