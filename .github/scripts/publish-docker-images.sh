c5752c9b70b00e5134ae681cb3cd3254cd90d7f1
Anubis <github@anubis.email>
2022-11-13 23:06:16 +0100

    Disable outdated CI

--- .github/scripts/publish-docker-images.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-SCALA_CLI_VERSION="$(./mill -i ci.publishVersion)"
-
-docker tag scala-cli virtuslab/scala-cli:latest
-docker tag scala-cli virtuslab/scala-cli:"$SCALA_CLI_VERSION"
-docker push virtuslab/scala-cli:latest
-docker push virtuslab/scala-cli:"$SCALA_CLI_VERSION"
